4f3d4c4baaf35f1e886b198bcfd02b5b352528f1
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-06 18:17:57 +0600

    header done.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='header'>
+            <div className='header-container'>
+                <div className='left'>
+                    <img src={logo} alt="" />
+                </div>
+                <div className='right'>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/shop'>Shop</Link></li>
+                        <li><Link to='/order'>Order</Link></li>
+                        <li><Link to='/orderreview'>Order Review</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/signup'>Signup</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
